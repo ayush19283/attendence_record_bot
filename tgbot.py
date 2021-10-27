160e0176e9fa148a1627fbc518dbb7c94cee182f
ayush19283 <89829211+ayush19283@users.noreply.github.com>
2021-10-27 15:14:21 +0530

    Add files via upload

---
+++ tgbot.py
@@ -0,0 +1,25 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler,InlineQueryHandler, Filters,CallbackContext
+from telegram import Update, Bot
+from datetime import date
+
+TOKEN = '2037652357:AAGJwNlXY2WBCeQTYNtEBbkmAxWbDjQ-zTg'
+mybot = Bot(TOKEN)
+d=date.today()
+d=str(d)
+def ab(update, context):
+    print(update.message.chat.id)
+    update.message.reply_text("hello")
+     
+    print(update.message.text)
+def date(update,context):
+    print(update.message.chat.id)
+    update.message.reply_text(d)
+    print(dir(update.message))
+    print(update.message.from_user.id)
+updater = Updater(TOKEN , use_context=True)
+dp = updater.dispatcher
+dp.add_handler(CommandHandler("hi", ab))
+dp.add_handler(CommandHandler("date",date))
+#dp.add_handler(MessageHandler(Filters.text&(~Filters.command), hi))
+updater.start_polling()
+updater.idle()
