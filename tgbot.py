31901ca2e1cb156c0cbd74738e6e41a7d71217d2
ayush19283 <89829211+ayush19283@users.noreply.github.com>
2021-11-24 20:05:13 +0530

    Update tgbot.py

--- tgbot.py
+++ tgbot.py
@@ -2,12 +2,13 @@ from telethon import TelegramClient,events,sync
 import psycopg2
 from telethon.sessions import StringSession
 from telethon import utils
+import os
 ##real_id, peer_type = utils.resolve_id
-api_id=8925256
-api_hash='d3a68074d2204bb004ca255cd009d337'
-str_sess='1BVtsOJgBu52QxyQPugSCETjHw-5YUvM6RB4mviNcpDEV3e0n0MW6aDlCgvi__m7DzL-lSESXjRK_OB_9l31rQ5n1uqhsoPbdXIij5uXiPRGvrH38g45e-Hd6a2WHvZUJjWORU7KowKpUzFSYhC1tBPjwyAFd7bSR0n2EvhxXhC-tPyB7Vv0V9TxBzdVl_3EjLzkPrIfQPGgQcRKTiXcSMiygXJqacx5MSftCqacc_uriHK7qHdat9fF81LMQKlQCmN_7WMTrKkSh2BgJAqMzxINCpc5WVqnjZl69MuLu7yVeRF0_x_LWAWXKoboIM1Z8vI0Cr-SGjVNzAV5pCFU1fiDcpfQie7Y='
+api_id=os.environ.get('api_id')
+api_hash=os.environ.get('api_hash')
+str_sess=os.environ.get('str_sess')
 client = TelegramClient(StringSession(str_sess), api_id, api_hash)
-conn = psycopg2.connect(database="d3ekcujj7lq6fc", user = "rqzgbqclathhzp", password = "65267788f7c2a21a536f469ae11870ff33324d515f4fc6e1fa784a99ac0f39ce", host = "ec2-3-217-91-165.compute-1.amazonaws.com", port = "5432")
+conn = psycopg2.connect(os.environ.get('DATABASE_URL'))
 cur = conn.cursor()
 
 ##peer = peer_type(real_id)
